ba2173e158b84e5d4ef4f39ff7b074f93f131dbb
k1ndrat <rubalkanaspining@gmail.com>
2024-03-23 16:06:53 +0200

    UPDATE | GoogleAuth(middleware)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,33 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const protectedRoutes = ["/profile", "/"];
 const authRoutes = ["/login"];
 
-export default function middleware(req: any) {
+export default async function middleware(req: NextRequest) {
   let tokens = {} as Tokens;
+
+  //
+  let refreshToken = req.nextUrl.searchParams.get("refreshToken");
+
+  if (refreshToken) {
+    const data = await fetch(
+      (process.env.NEXT_PUBLIC_BASE_URL + "/auth/refresh") as string,
+      {
+        method: "POST",
+        headers: {
+          authorization: `REFRESH ${refreshToken}`,
+        },
+      }
+    );
+
+    const res = await data.json();
+
+    const response = NextResponse.redirect(req.nextUrl.origin);
+    response.cookies.set("tokens", JSON.stringify(res));
+    return response;
+  }
+
+  // cookies tokens
   if (req.cookies.get("tokens")?.value) {
     tokens = JSON.parse(req.cookies.get("tokens")?.value as string);
   }
@@ -18,10 +41,4 @@ export default function middleware(req: any) {
     const absoluteUrl = new URL("/", req.nextUrl.origin);
     return NextResponse.redirect(absoluteUrl.toString());
   }
-
-  // if (!isAuth() && protectedRoutes.includes(req.nextUrl.pathname)) {
-  //   const absoluteUrl = new URL("/login", req.nextUrl.origin);
-
-  //   return NextResponse.redirect(absoluteUrl.toString());
-  // }
 }
